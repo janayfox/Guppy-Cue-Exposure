1c39c98810f9c1dac480b430100e817061529fd5
Janay <janayfox95@gmail.com>
2025-07-01 20:18:28 -0400

    finalize folder

--- zip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=30
-#SBATCH --mem-per-cpu=4G
-#SBATCH --time=0-10:00:00
-#SBATCH --account=def-barrett
-
-tar -zcvf dev.tar.gz ./SRA/dev 
-tar -zcvf st.tar.gz ./SRA/st 
